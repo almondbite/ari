15e73f3192c6fad4b584317f1a4430280e0351eb
Nam Nguyễn <148507165+almondbite@users.noreply.github.com>
2024-07-12 08:44:56 -0500

    Added

---
+++ ari.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "defs.h"
+
+int main(){
+
+    AllInit();
+    
+    int PieceOne = rand();
+    int PieceTwo = rand();
+    int PieceThree = rand();
+    int PieceFour = rand();
+
+    printf("PieceOne:%X\n", PieceOne);
+    printf("PieceTwo:%X\n", PieceTwo);
+    printf("PieceThree:%X\n", PieceThree);
+    printf("PieceFour:%X\n", PieceFour);
+
+    int Key = PieceOne ^ PieceTwo ^ PieceThree ^ PieceFour;
+    int TempKey = PieceTwo;
+    TempKey ^= PieceThree;
+    TempKey ^= PieceFour;
+    TempKey ^= PieceOne;
+
+    printf("Key:%X\n", Key);
+    printf("TempKey:%X\n", TempKey);
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	gcc ari.c init.c bitboards.c hashkeys.c -o ari
